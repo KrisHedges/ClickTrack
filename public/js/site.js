79b69aa9c623076f67c7987fc3390f1b7fb874e2
Kris Hedges <KrisHedges@gmail.com>
2011-01-27 02:38:57 -0500

    Ok so that didn't work... perhaps we'll worry about that crap some other day removed the touchhandler and supporting js

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-    touchinit();
-
     $("#chart").live("click", function(mouse) {
 	var x = mouse.pageX - this.offsetLeft;
 	var y = mouse.pageY - this.offsetTop;
@@ -29,32 +27,4 @@ $(document).ready(function() {
 	var points = $("#chart").html();
 	alert(points);
     });
-
-    function touchHandler(event) {
-     var touches = event.changedTouches,
-      first = touches[0],
-      type = "";
-      switch(event.type) {
-       case "touchstart": type = "mousedown"; break;
-       case "touchmove":  type="mousemove"; break;        
-       case "touchend":   type="mouseup"; break;
-       default: return;
-      }
-
-  var simulatedEvent = document.createEvent("MouseEvent");
-   simulatedEvent.initMouseEvent(type, true, true, window, 1, 
-    first.screenX, first.screenY, 
-    first.clientX, first.clientY, false, 
-    false, false, false, 0/*left*/, null);
-
-   first.target.dispatchEvent(simulatedEvent);
-   event.preventDefault();
-  }
-
-  function touchinit() {
-   document.addEventListener("touchstart", touchHandler, true);
-   document.addEventListener("touchmove", touchHandler, true);
-   document.addEventListener("touchend", touchHandler, true);
-   document.addEventListener("touchcancel", touchHandler, true);
-  }
 });

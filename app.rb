6624352c6476cd8177b8635cd839b1493d2701d1
Kris Hedges <KrisHedges@gmail.com>
2011-01-29 04:12:06 -0500

    Much of the core JS has been put in place though certainly with room for improvement and a refactoring is mosdef needed.

--- app.rb
+++ app.rb
@@ -1,34 +1,54 @@
-['rubygems', 'sinatra', 'dm-core' , 'dm-validations' , 'haml'].each{|r|require r}
-
-## CONFIGURATION
-DataMapper.setup(:default, {
- :adapter  => 'sqlite3',
- :host     => 'localhost',
- :username => '' ,
- :password => '',
- :database => 'db/app.db'})
-
+['rubygems', 'sinatra', 'dm-core' , 'dm-validations' ,'dm-migrations', 'haml', 'json'].each{|r|require r}
 
 ## SET HAML > HTML5
 set :haml, :format => :html5
 
+## CONFIGURATION
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/development.db")
+
 ## MODELS
 class Chart
-  include DataMapper::Resource
-  property :id,         Serial
-  property :points,     Text
-  property :created_at, DateTime
-
-  validates_presence_of :points
+  include DataMapper::Resource  
+  property :id,           Serial
+  property :points,       Text
+  property :created_at,   DateTime
 end
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 ##URI's
-
 get '/' do
+  @charts = Chart.all :order => [:created_at.desc]
+  @chartcount = @charts.size
   haml :index                   
 end
 
-post '/' do
-  chart = Chart.create(:points=>params[:points],:created_at=>Time.now)
-  redirect '/'
+get '/charts' do
+ @charts = Chart.all :order => [:created_at.desc]
+ @chartcount = @charts.size
+ haml :list
+end
+
+get '/chart/:id' do
+  @chart = Chart.get(params[:id])
+  haml :show
+end
+
+post '/chart/new' do
+ chart = Chart.create(:points=>params[:points],:created_at=>Time.now)
+ if chart.save
+   status 201
+ else
+   status 412
+ end
+end
+
+post '/chart/destroy/:id' do
+  @chart = Chart.get(params[:id])
+  @chart.destroy
+  if @chart.destroy
+    status 201
+  else
+    status 412
+  end
 end
--- public/js/site.js
+++ public/js/site.js
@@ -1,30 +1,131 @@
 $(document).ready(function() {
 
-    $("#chart").live("click", function(mouse) {
-	var x = mouse.pageX - this.offsetLeft;
-	var y = mouse.pageY - this.offsetTop;
-	$("#chart").append('<div class="point" style="top:'+ y +'px;left:'+ x +'px;"></div>');
-    });
-
-    $("div.point").live('mouseover mouseout', function(ShowPoint) {
-	if (ShowPoint.type == 'mouseover') {
-	    $(this).addClass("highlight");
-	} else {
-	    $(this).removeClass("highlight");
-	}
-    });
-
-    $(".point").live("click", function() {
-	$(this).fadeOut('fast');
+    function clearChart() {
+        $(".point").fadeOut('slow',
+        function() {
+            $(this).remove();
+        });
+	$("#chart-date").html("Create a new chart now or browse the chart history");
 	return false;
+    };
+
+    function flashNotice(notice) {
+        $(".noticemsg").replaceWith("<span class='noticemsg'>" + notice + "</span>");
+        $("#notice").fadeIn('fast').delay(3000).fadeOut('slow');
+	return false;
+    };
+    
+    function updateList() {
+	$.ajax({
+	    type: "GET",
+	    url: "/charts",
+	    datatype: "html",
+	    success: function(l) {
+		$('#chart-history').html(l);
+	    }
+	}),
+	clearChart();
+	return false;
+    };
+
+    function showList() {
+	$.ajax({
+	    type: "GET",
+	    url: "/charts",
+	    datatype: "html",
+	    success: function(list) {
+		$('#chart-history').html(list);
+	    }
+	})
+    };
+                                                 
+//Init
+     showList();
+
+    $("#chart").live("click",
+    function(mouse) {
+        var x = mouse.pageX - this.offsetLeft;
+        var y = mouse.pageY - this.offsetTop;
+        $("#chart").append('<div class="point" style="top:' + y + 'px;left:' + x + 'px;"></div>');
     });
 
-    $("#clearchart").bind("click", function() {
-	$(".point").fadeOut('slow');
+    $("div.point").live('mouseover mouseout',
+    function(ShowPoint) {
+        if (ShowPoint.type == 'mouseover') {
+            $(this).addClass("highlight");
+        } else {
+            $(this).removeClass("highlight");
+        }
     });
 
-    $("#save").bind("click", function(){
-	var points = $("#chart").html();
-	alert(points);
+    $(".point").live("click",
+    function() {
+        $(this).fadeOut('fast',
+        function() {
+            $(this).remove();
+        });
+        return false;
     });
+
+    $("#clearchart").bind("click",
+    function() {
+        clearChart();
+    });
+
+    $("#save").bind("click", function() {
+    var displayedpoints = $("#chart").html();
+	$.ajax({
+	    type: "POST",
+	    processData: "false",
+	    url: "/chart/new",
+	    datatype: "html",
+	    data: {points: displayedpoints},
+	    success: function() {
+		updateList();
+		flashNotice("Your chart has been saved");
+	    },
+	    error: function() {
+		flashNotice("There was a problem saving your chart.");
+	    }
+	});
+    });
+    
+    $(".chart").live("click", function(e) {
+	var chartid = $(this).attr("id");
+	var chartdate = $(this).attr("title");
+	e.preventDefault();
+	$.ajax({
+	    type: "GET",
+	    url: "chart/"+chartid,
+	    datatype: "html",
+	    success: function(response){
+		clearChart();
+		$('#chart').append(response).fadeIn('slow');
+		$("#chart-date").html(chartdate);
+	    },
+	    error: function(){
+		flashNotice("Problem retrieving that chart");
+	    }
+	});
+    });
+    
+    $(".destroy").live("click", function(e) {
+	var chartid = $(this).attr("id");
+	var chartdate = $(this).attr("title");
+	e.preventDefault();
+	$.ajax({
+	    type: "POST",
+	    url: "chart/destroy/"+chartid,
+	    datatype: "html",
+	    success: function(response){
+		clearChart();
+		updateList();
+		flashNotice("Chart from " + chartdate + " has been Deleted.");
+	    },
+	    error: function(){
+		flashNotice("There's been a problem deleting that chart.");
+	    }
+	});
+    });
+
 });

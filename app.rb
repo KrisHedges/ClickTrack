6805e8f10caea2412d7d373008c442b7fa4b5d7a
Kris Hedges <KrisHedges@gmail.com>
2011-01-29 04:26:10 -0500

    nope that was very bad

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-['rubygems', 'sinatra', 'dm-core' , 'dm-validations' ,'dm-migrations','dm-postgres-adapter', 'haml'].each{|r|require r}
+['rubygems', 'sinatra', 'dm-core' , 'dm-validations' ,'dm-migrations', 'haml'].each{|r|require r}
 
 ## SET HAML > HTML5
 set :haml, :format => :html5
